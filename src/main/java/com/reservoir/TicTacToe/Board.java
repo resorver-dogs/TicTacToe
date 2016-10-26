e54ed8d958df695ec6f664efd49c8ff647ff73fa
Skúli Þór <skulij11@ru.is>
2016-10-26 01:22:32 +0000

    first test and result - making char[3][3] board

--- src/main/java/com/reservoir/TicTacToe/Board.java
+++ src/main/java/com/reservoir/TicTacToe/Board.java
@@ -1,5 +1,15 @@
 package com.reservoir.TicTacToe;
 
 public class Board {
+
+    private char[][] board;
+
+    public Board() {
+        this.board = new char[3][3];
+
+    }
+    public char[][] getBoard() {
+        return this.board;
+    }
     
 }
--- src/test/java/com/reservoir/TicTacToe/BoardTest.java
+++ src/test/java/com/reservoir/TicTacToe/BoardTest.java
@@ -1,10 +1,17 @@
 package com.reservoir.TicTacToe;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.Rule;
 
 public class BoardTest {
+        
+        @Test
+        public void boardCorrectlyBuilt() {
+            Board board = new Board();
+            char[][] charBoard = new char[3][3];
+            assertArrayEquals(board.getBoard(), charBoard);        
+        }
 
 }
